25a0deac36fd8e48a0bd288bf7a3187cf0914d4b
fabian <fpiceno@urrea.com.mx>
2019-01-02 16:52:29 -0600

    servicios para productos

--- portal/src/main/java/com/fpiceno/portal/controller/ProductoController.java
+++ portal/src/main/java/com/fpiceno/portal/controller/ProductoController.java
@@ -1,12 +1,30 @@
 package com.fpiceno.portal.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.fpiceno.portal.service.IUserService;
+import com.fpiceno.portal.service.ProductoService;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
 	
+	@Autowired
+	private  ProductoService service;
 	
+	@RequestMapping(value="/home")
+	public String home(ModelMap model, Authentication authentication) {
+		authentication.getPrincipal();
+		model.addAttribute("productos", service.getProducts());
+ 		return "user-info";
+ 	}
+	@RequestMapping(value="/error")
+	public String error() {
+ 		return "access-denied";
+ 	}
 
 }
--- portal/src/main/java/com/fpiceno/portal/service/ProductoService.java
+++ portal/src/main/java/com/fpiceno/portal/service/ProductoService.java
@@ -1,6 +1,15 @@
 package com.fpiceno.portal.service;
 
+import java.util.List;
+
+import org.springframework.security.access.annotation.Secured;
+
+import com.fpiceno.portal.entity.Producto;
+
 public interface ProductoService {
+
+	@Secured ({"ROLE_ADMIN"})
+	List<Producto>  getProducts();
 	
 	
 	
